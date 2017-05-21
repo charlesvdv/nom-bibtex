7e8028e5ab2d5d42136cb5d4e104938cda443030
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-05-21 23:09:17 +0200

    fix formatting

--- tests/bib.rs
+++ tests/bib.rs
@@ -18,16 +18,15 @@ some comments
 
 #[test]
 fn test_bibtex() {
-    let biblio = BibliographyEntry::new("misc", "knuthwebsite", vec![
+    let biblio = BibliographyEntry::new("misc",
+                                        "knuthwebsite",
+                                        vec![
         ("author".into(), "Donald Knuth".into()),
         ("title".into(), "Knuth: Computers and Typesetting".into()),
         ("url".into(), "http://www-cs-faculty.stanford.edu/\\~{}uno/abcde.html".into())
     ]);
     assert_eq!(Bibtex::parse(BIBDATA).unwrap(),
-               Bibtex::new(vec![
-                           Entry::Bibliography(biblio),
-                           Entry::Variable("key".into(), "value".into()),
-                           Entry::Comment("some comments".into())
-               ])
-    );
+               Bibtex::new(vec![Entry::Bibliography(biblio),
+                                Entry::Variable("key".into(), "value".into()),
+                                Entry::Comment("some comments".into())]));
 }
