3436e129a743186095655e120cb77a345ca0206c
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-05-22 12:45:00 +0200

    add benches

---
+++ benches/parser.rs
@@ -0,0 +1,17 @@
+#![feature(test)]
+
+extern crate nom_bibtex;
+extern crate test;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+#[bench]
+fn bench_parser(b: &mut test::Bencher) {
+    let mut file = File::open("samples/bench.bib").unwrap();
+    let mut bib_content = String::new();
+
+    file.read_to_string(&mut bib_content).unwrap();
+
+    b.iter(|| nom_bibtex::Bibtex::parse(&bib_content));
+}
