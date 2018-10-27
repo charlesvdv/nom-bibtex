a0f29c7cbf4f9f148d1e1894e3159a65f88e7552
Ruben De Smet <ruben.de.smet@rubdos.be>
2018-10-27 23:51:42 +0200

    cleanup BibtexError::from

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 use nom::Err;
-use nom::types::CompleteByteSlice;
 
 quick_error! {
     #[derive(Debug, PartialEq, Eq)]
@@ -8,29 +7,22 @@ quick_error! {
         Parsing (descr: String) {
             description(descr)
             display(me) -> ("Parsing error. Reason: {}", me.description())
-            // TODO: how can we group the below two?
-            from(err: Err<CompleteByteSlice<'_>>) -> (
-                match err {
-                    Err::Incomplete(e) => format!("Incomplete: {:?}", e),
-                    Err::Error(e) | Err::Failure(e) => e.into_error_kind().description().into(),
-                }
-            )
-            from(err: Err<&[u8]>) -> (
-                match err {
-                    Err::Incomplete(e) => format!("Incomplete: {:?}", e),
-                    Err::Error(e) | Err::Failure(e) => e.into_error_kind().description().into(),
-                }
-            )
-            from(err: Err<&str>) -> (
-                match err {
-                    Err::Incomplete(e) => format!("Incomplete: {:?}", e),
-                    Err::Error(e) | Err::Failure(e) => e.into_error_kind().description().into(),
-                }
-            )
         }
         StringVariableNotFound (var: String) {
             description("String variable not found.")
             display(me) -> ("{}: {}", me.description(), var)
         }
+    }
+}
+
+// We cannot use the from() from quick_error, because we need to put lifetimes that we didn't
+// define.
+impl<T> From<Err<T>> for BibtexError {
+    fn from(err: Err<T>) -> BibtexError {
+        let descr = match err {
+            Err::Incomplete(e) => format!("Incomplete: {:?}", e),
+            Err::Error(e) | Err::Failure(e) => e.into_error_kind().description().into(),
+        };
+        BibtexError::Parsing(descr)
     }
 }
